04fb86c430f502e6673d75ee6e1ffee02da301f7
Saroj Belbase <siddharthbelbase@gmail.com>
2024-05-28 07:20:23 +0000

    feat: uses vue router scroll into view instead of library

--- src/components/portfolio/navbar.vue
+++ src/components/portfolio/navbar.vue
@@ -1,37 +1,36 @@
 <template>
-              <nav class="navbar navbar-expand-lg navbar-dark darker fixed-top" id="sideNav">
-            <a class="navbar-brand" href="#page-top">
-                <span class="d-block d-lg-none">Saroj Belbase</span>
-                <span class="d-none d-lg-block">
-                    <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="../../assets/img/profile.jpg"
-                        alt="sarojbelbase">
-                </span>
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#about" v-scroll-to="'#about'">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#skills" v-scroll-to="'#skills'">Skills</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#interests" v-scroll-to="'#interests'">Interests</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#projects" v-scroll-to="'#projects'">Projects</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#education" v-scroll-to="'#education'">Education</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#experiences" v-scroll-to="'#experiences'">Experiences</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-</template>
+    <nav class="navbar navbar-expand-lg navbar-dark darker fixed-top" id="sideNav">
+      <a class="navbar-brand" href="#page-top">
+        <span class="d-block d-lg-none">Saroj Belbase</span>
+        <span class="d-none d-lg-block">
+          <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="../../assets/img/profile.jpg" alt="Saroj Belbase | sarojbelbase" title="Saroj Belbase | sarojbelbase">
+        </span>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{ path: '/', hash: '#about' }">About</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{ path: '/skills', hash: '#skills' }">Skills</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{ path: '/interests', hash: '#interests' }">Interests</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{ path: '/projects', hash: '#projects' }">Projects</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{ path: '/education', hash: '#education' }">Education</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{ path: '/experiences', hash: '#experiences' }">Experiences</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </template>
+  
--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,59 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import resume from '../views/resume.vue'
+import about from "@/components/portfolio/about";
+import skills from "@/components/portfolio/skills";
+import interests from "@/components/portfolio/interests";
+import projects from "@/components/portfolio/projects";
+import education from "@/components/portfolio/education";
+import experiences from "@/components/portfolio/experiences";
 
 const routes = [
   {
     path: '/',
-    name: 'resume',
-    component: resume
+    name: 'about',
+    component: about
+  },
+  {
+    path: '/skills',
+    name: 'skills',
+    component: skills
+  },
+  {
+    path: '/interests',
+    name: 'interests',
+    component: interests
+  },
+  {
+    path: '/projects',
+    name: 'projects',
+    component: projects
+  },
+  {
+    path: '/education',
+    name: 'education',
+    component: education
+  },
+  {
+    path: '/experiences',
+    name: 'experiences',
+    component: experiences
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: 'smooth'
+      };
+    } else {
+      return { top: 0 };
+    }
+  }
 })
 
 export default router
