4bdf4e4f7af710fc186807e6ac3498a6778089c0
Saroj Belbase <siddharthbelbase@gmail.com>
2024-05-28 07:10:07 +0000

    feat: add h1 and h2 tags

--- src/components/portfolio/about.vue
+++ src/components/portfolio/about.vue
@@ -1,5 +1,11 @@
 <template>
   <section class="resume-content p-3 p-lg-5 d-flex align-items-center" id="about">
+    <h1 class="d-none m-0 p-0">
+      Saroj Belbase | FullStack Developer | Nepali Python Developer | Nepali NodeJs Developer | Software Engineer
+    </h1>
+    <h2 class="d-none m-0 p-0">
+      Saroj Belbase is a full-stack developer known for his work with Python and Node.js. His portfolio showcases a variety of projects, including an automated bot that scrapes news and investment opportunities in Nepal, a collection of smart utilities and helper functions, a Telegram bot for Nepal Banda updates, a browser extension for a distraction-free Nepali date, and a Flask app for checking numbers across Nepal's carriers. Although there is no recent activity listed for this period, his work reflects a diverse range of skills and interests in the tech industry.
+    </h2>
     <div class="w-100">
       <p class="normal mb-1 text-darker">Hi, I am</p>
       <h1 class="display-1 font-weight-bold mb-0">
